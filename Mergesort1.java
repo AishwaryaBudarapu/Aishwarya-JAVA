d67b00ed400bb523053d72200d98a6321e2f5684
AishwaryaBudarapu <168456435+AishwaryaBudarapu@users.noreply.github.com>
2024-06-28 09:48:58 +0530

    Add files via upload

---
+++ Mergesort1.java
@@ -0,0 +1,62 @@
+import java.util.*;
+class Mergesort1
+{
+    public static void main(String[] args) 
+    {
+      Scanner sc =new Scanner(System.in);
+      int m=sc.nextInt();
+      int a[]=new int[m];
+      int i,j,k;
+      for(i=0;i<m;i++)
+      {
+          a[i]=sc.nextInt();
+      }
+      int n=sc.nextInt();
+      int b[]=new int[n];
+      for(j=0;j<n;j++)
+      {
+        b[j]=sc.nextInt();
+      }
+int c[]=new int[m+n];
+i=0;
+j=0;
+k=0;
+while(i<m && j<n)
+{
+      if(a[i]<b[j])
+{
+   c[k]=a[i];
+   i++;
+   k++;
+}
+else
+{
+   c[k]=b[j];
+   j++;
+   k++;
+}
+
+while(i<m)
+{
+c[k]=a[i];
+i++;
+k++;
+}
+while(j<n)
+{
+c[k]=b[j];
+j++;
+k++;
+}
+}
+for(i=0;i<k;i++)
+{
+System.out.print(c[i]+" ");
+}
+}
+}
+
+
+
+
+     
